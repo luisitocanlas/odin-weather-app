bae367fabb7320653a85516b22f949bd3b7e3e08
Luisito Canlas <luisitocanlas@gmail.com>
2024-06-12 13:55:27 -0700

    Modified initial setup

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,4 +1,3 @@
-// config/webpack.common.js
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-// src/index.js
 import './style.css';
 
 console.log('Hello, Webpack!');
