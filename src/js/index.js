d0f56a09540951c1cbffa78d7320097ecfeb66d2
Luisito Canlas <luisitocanlas@gmail.com>
2024-07-02 16:30:59 -0700

    Updated with fetching the data and logging the data

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,14 @@
-import './style.css';
+import '../css/style.css';
+import WeatherAPI from './api';
+import WeatherDisplay from '../components/WeatherDisplay';
 
-console.log('Hello, Webpack!');
+const apiKey = process.env.WEATHER_API_KEY;
+const weatherAPI = new WeatherAPI(apiKey);
+const weatherDisplay = new WeatherDisplay(document.body);
+
+async function loadWeather() {
+	const weatherData = await weatherAPI.fetchWeather('Bellevue');
+	weatherDisplay.render(weatherData);
+}
+
+loadWeather();
