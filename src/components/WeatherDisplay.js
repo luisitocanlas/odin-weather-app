b86ebf88c24d05017d959b688af3496f2247fa8a
Luisito Canlas <luisitocanlas@gmail.com>
2024-07-02 16:48:12 -0700

    Updated to show data instead of console logging

--- src/components/WeatherDisplay.js
+++ src/components/WeatherDisplay.js
@@ -4,6 +4,20 @@ export default class WeatherDisplay {
 	}
 
 	render(weatherData) {
-		console.log(weatherData);
+		const { location, current } = weatherData;
+
+		this.container.innerHTML = `
+      <h2>Weather in ${location.name}, ${location.region}, ${location.country}</h2>
+      <p>${location.localtime}</p>
+      <div class="weather-condition">
+        <img src="${current.condition.icon}" alt="${current.condition.text}">
+        <p>${current.condition.text}</p>
+      </div>
+      <p>Temperature: ${current.temp_c}°C (${current.temp_f}°F)</p>
+      <p>Feels like: ${current.feelslike_c}°C (${current.feelslike_f}°F)</p>
+      <p>Humidity: ${current.humidity}%</p>
+      <p>Wind: ${current.wind_kph} kph (${current.wind_mph} mph) ${current.wind_dir}</p>
+      <p>Precipitation: ${current.precip_mm} mm (${current.precip_in} in)</p>
+    `;
 	}
 }
