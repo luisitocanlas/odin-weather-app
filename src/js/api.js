ad0029cfaae1d855600832fe657b294e1ab041e3
Luisito Canlas <luisitocanlas@gmail.com>
2024-07-02 18:20:56 -0700

    Updated for multi line cities

--- src/js/api.js
+++ src/js/api.js
@@ -5,8 +5,9 @@ export default class WeatherAPI {
 	}
 
 	async fetchWeather(city) {
+		const encodedCity = encodeURIComponent(city);
 		try {
-			const response = await fetch(`${this.baseUrl}?city=${city}`);
+			const response = await fetch(`${this.baseUrl}?city=${encodedCity}`);
 			if (!response.ok) {
 				throw new Error('Network response was not ok');
 			}
@@ -19,31 +20,4 @@ export default class WeatherAPI {
 			);
 		}
 	}
-
-	// processWeatherData(data) {
-	// 	return {
-	// 		location: {
-	// 			name: data.location.name,
-	// 			region: data.location.region,
-	// 			country: data.location.country,
-	// 			localtime: data.location.localtime,
-	// 		},
-	// 		current: {
-	// 			temp_c: data.current.temp_c,
-	// 			temp_f: data.current.temp_f,
-	// 			condition: {
-	// 				text: data.current.condition.text,
-	// 				icon: `http:${data.current.condition.icon}`,
-	// 			},
-	// 			humidity: data.current.humidity,
-	// 			wind_kph: data.current.wind_kph,
-	// 			wind_mph: data.current.wind_mph,
-	// 			wind_dir: data.current.wind_dir,
-	// 			precip_mm: data.current.precip_mm,
-	// 			precip_in: data.current.precip_in,
-	// 			feelslike_c: data.current.feelslike_c,
-	// 			feelslike_f: data.current.feelslike_f,
-	// 		},
-	// 	};
-	// }
 }
